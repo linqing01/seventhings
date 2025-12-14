7bb4f02205f550dd10215f431597e82e1fc6a0a0
linqing <linqing01@gmail.com>
2025-12-14 11:25:14 +0800

    upgrade to liftweb 4.0.0-M3

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -4,15 +4,13 @@ import net.liftweb._
 import common._
 import http._
 import sitemap._
-import net.liftmodules.JQueryModule
-
 
 /**
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
 class Boot {
-  def boot {
+  def boot: Unit = {
     // where to search snippet
     LiftRules.addToPackages("net.liftweb.seventhings")
 
@@ -58,8 +56,5 @@ class Boot {
               ContentSourceRestriction.Self)
           )))
     }
-
-    JQueryModule.InitParam.JQuery=JQueryModule.JQuery224
-    JQueryModule.init()
   }
 }
--- src/main/scala/net/liftweb/seventhings/snippet/LongTime.scala
+++ src/main/scala/net/liftweb/seventhings/snippet/LongTime.scala
@@ -9,12 +9,12 @@ import Helpers._
  * Do something that takes a long time to do
  */
 object LongTime {
-  def render = {
+  def render: CssSel = {
     // capture the start time
     val start = now
 
     // sleep for up to 15 seconds
-    Thread.sleep(randomLong(15 seconds))
+    Thread.sleep(randomLong(15.seconds))
 
     // send the result back
     "#start" #> start.toString &
